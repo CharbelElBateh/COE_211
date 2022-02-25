a123e7210767f8912ea5e977743dd77995e09da8
CharbelElBateh <99130418+CharbelElBateh@users.noreply.github.com>
2022-02-25 20:29:14 +0200

    Add files via upload

---
+++ StaffManagment/Employee.java
@@ -0,0 +1,14 @@
+public class Employee{
+	String Fname;
+	String Lname;
+	int age;
+	double salary;
+
+	Employee ( String Fname, String Lname, int age, double salary){
+	this.Fname = Fname;
+	this.Lname = Lname;
+	this.age = age;
+	this.salary = salary;
+	}
+	
+}
